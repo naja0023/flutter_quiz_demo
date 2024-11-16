4e683575b04e32d327b5535599bedf30dc577aa2
MoyanyosS <naja.67130@gmail.com>
2024-11-16 20:32:28 +0700

    remove bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_welcome/di/get_it.dart';
-import 'package:flutter_welcome/screen/question_screen/bloc/question_bloc.dart';
 import 'package:flutter_welcome/screen/question_screen/provider/question_provider.dart';
 import 'package:flutter_welcome/screen/root_screen/provider/root_provider.dart';
 import 'package:provider/provider.dart';
@@ -24,7 +22,6 @@ class QuizApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider<RootProvider>(create: (context) => RootProvider()),
         ChangeNotifierProvider<QuestionProvider>(create: (context) => QuestionProvider()),
-        BlocProvider<QuestionBloc>(create: (context) => QuestionBloc()),
       ],
       child: MaterialApp(
         navigatorKey: getIt<NavigationService>().navigatorKey,
